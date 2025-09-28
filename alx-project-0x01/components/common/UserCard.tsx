4d8c438f0a103a429c68110d200b8be76907dfac
larryQuao <honyadjilawrence2s1@gmail.com>
2025-09-28 13:32:14 +0000

    Update users page and interfaces, add UserCard component

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,19 @@
+import { UserProps } from "@/interfaces";
+
+const UserCard: React.FC<UserProps> = ({ id, name, username, email, address, phone, website, company }) => {
+  return (
+    <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+      <div className="mb-4">
+        <h2 className="text-2xl font-semibold text-gray-800">{id}</h2>
+        <h2 className="text-2xl font-semibold text-gray-800">{name} ({username})</h2>
+        <p className="text-gray-600">{email}</p>
+        <p className="text-gray-600">{address.street}, {address.city}, {address.zipcode}</p>
+        <p className="text-gray-600">{phone}</p>
+        <p className="text-gray-600">{website}</p>
+        <p className="text-gray-600">{company.name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,10 +1,38 @@
-const User: React.FC = () => {
+import { UserProps } from "@/interfaces";
+import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+
+const User: React.FC<UserProps[]> = ({ users }) => {
+    console.log(users);
     return (
-        <div className="p-4 border-b border-gray-200">
-            <h2 className="text-xl font-bold">User Name</h2>
-            <p className="text-gray-600">User details go here...</p>
-        </div>
+        <div className="flex flex-col h-screen">
+        <Header />
+        <main className="p-4">
+            <div className="flex justify-between">
+                <h1 className="text-2xl font-semibold">User Details</h1>
+                <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
+            </div>
+            <div className="grid grid-cols-3 gap-2">
+                {
+                    users?.map(({ id, name, username, email, address, phone, website, company }: UserProps, key: number) => (
+                        <UserCard  id={id} username={username} name={name} email={email} address={address} phone={phone} website={website} company={company} key={key}  />
+                    ))
+                }
+            </div>
+        </main>
+    </div>
     );
 };
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const users = await response.json()
+
+  return {
+    props: {
+      users
+    }
+  }
+}
 
 export default User;
